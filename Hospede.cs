f3e7c629821a83b51968c45af5b8e3f3babde85b
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-10-27 01:18:16 -0300

    Add files via upload
    VEJA O CONTEÚDO DISPONÍVEL NA AULA DO DIA 19/10/20;
    
    Enunciado do exercício:
    Um pensionato tem dez quartos para alugar, sendo que esses quartos são identificados por números de 0 a 9. Desenvolva um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de hospedes que irão alugar os quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N hospedes, assim  como o quarto que ele escolheu (0 a 9). Suponha que seja escolhido um quarto vago. Ao termino, o seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto.

---
+++ Hospede.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_TarefaVetoresHotel
+{
+    class Hospede
+    {
+        public string Nome { get; set; }    //Recebe e grava o nome do hóspede
+        public string Email { get; set; }   //Recebe e grava o e-mail do hóspede
+        public Hospede(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace POO_TarefaVetoresHotel
+//Hotel com 10 quartos para alugar.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criação do vetor Hospede, limitando em 10
+            Hospede[] vect = new Hospede[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());  //Recebe do usuário a informação de quantidade de hóspedes a receber
+            for (int i = 1; i <= n; i++)    //Faz um laço para solicitar os dados conforme a quantidade de hóspedes informada 
+            {
+                Console.WriteLine();
+                Console.WriteLine("Hóspede: "+i);   //Informa a sequência de entrada dos hóspedes
+                Console.Write("Nome: ");            //Solicita o nome do hóspede
+                string nome = Console.ReadLine();   
+                Console.Write("Email: ");           //Solicita seu e-mail
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");          //Solicita o número do quarto onde será hospedado
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Hospede(nome, email);    //Vetor quarto recebe os nomes e e-mails dos hóspedes
+            }
+            Console.WriteLine();
+            //Informa a ocupação dos quartos de forma ordenada
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)    //Faz um laço para verficar os quartos ocupados
+            {
+                if (vect[i] != null)    //Se o vetor não estiver vazio, informará o conteúdo dele, sequencialmente
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+            Console.WriteLine("============================");
+
+            Console.WriteLine("Ocupação Geral dos Quartos:");
+            for (int i = 0; i < 10; i++)    //Faz um laço para verficar os quartos ocupados
+            {
+                Console.WriteLine(i + ": " + vect[i]);
+            }
+        }
+    }
+}
